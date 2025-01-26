57f4d1ba8e87712cc61acd663544a489b4733d55
kspace-trk <keigonakao.0825@gmail.com>
2025-01-26 14:53:28 +0900

    chore: WeatherAPI型定義作成

---
+++ types/WheatherAPI.ts
@@ -0,0 +1,68 @@
+export type WeatherAPI = {
+  publicTime: string
+  publicTimeFormatted: string
+  publishingOffice: string
+  title: string
+  link: string
+  description: {
+    publicTime: string
+    publicTimeFormatted: string
+    headlineText: string
+    bodyText: string
+    text: string
+  }
+  forecasts: {
+    date: string
+    dateLabel: string
+    telop: string
+    detail: {
+      weather: string | null
+      wind: string | null
+      wave: string | null
+    }
+    temperature: {
+      min: {
+        celsius: string | null
+        fahrenheit: string | null
+      }
+      max: {
+        celsius: string | null
+        fahrenheit: string | null
+      }
+    }
+    chanceOfRain: {
+      T00_06: string
+      T06_12: string
+      T12_18: string
+      T18_24: string
+    }
+    image: {
+      title: string
+      url: string
+      width: number
+      height: number
+    }
+  }[]
+  location: {
+    area: string
+    prefecture: string
+    district: string
+    city: string
+  }
+  copyright: {
+    title: string
+    link: string
+    image: {
+      title: string
+      link: string
+      url: string
+      width: number
+      height: number
+    }
+    provider: {
+      link: string
+      name: string
+      note: string
+    }[]
+  }
+}
